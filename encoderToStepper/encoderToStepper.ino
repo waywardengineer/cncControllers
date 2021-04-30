72a9e76a62db5faafa38f6a61029d4b9ff78ca41
ray <waywardengineer@gmail.com>
2021-04-29 18:31:03 -0700

    Better low speed control hopefully

--- encoderToStepper/encoderToStepper.ino
+++ encoderToStepper/encoderToStepper.ino
@@ -16,12 +16,24 @@ bool aPinState;
 bool bPinState;
 const int8_t directionFlip = 1;
 int stepsToGo = 0;
-const int stepsPerTurn = 15;
+int stepsPerNotch = 1;
+long timeOfLastStop;
+long timeSinceLastStop = 1000;
+long nextStepMicrosecondsTime = 0;
+
+const int minStepTime = 150;
+const int maxStepTime = 500;
+
+const int stepsToGoRampStart = 10;
+const int stepsToGoRampEnd = 800;
+
 void setup() {
   pinMode (APin, INPUT);
   pinMode (BPin, INPUT);  
   pinMode (directionPin, OUTPUT);
   pinMode (pulsePin, OUTPUT);
+  timeOfLastStop = millis();
+  
 
 }
 
@@ -41,35 +53,44 @@ void loop() {
   else{
     thisState = stateNoneTriggered;
   }
-  if (lastState == stateNoneTriggered && thisState == stateATriggered){
-    stepsToGo += stepsPerTurn * directionFlip;
-  }
-  if (lastState == stateNoneTriggered && thisState == stateBTriggered){
-    stepsToGo -= stepsPerTurn * directionFlip;
-  }
-  if (stepsToGo < 0){
-    digitalWrite(directionPin, LOW);
-  }
-  else {
-    digitalWrite(directionPin, HIGH);
-  }
-  int absSteps = abs(stepsToGo);
-  if (absSteps > 0){
-    if (absSteps > 400){
-      delayTime = 125;
+
+  if (lastState != stateNoneTriggered && thisState == stateNoneTriggered){
+    long timeOfThisStop = millis();
+    timeSinceLastStop = timeOfThisStop - timeOfLastStop;
+    timeOfLastStop = timeOfThisStop;
+    if (timeSinceLastStop < 100){
+      stepsPerNotch = 15;
     }
-    else if (absSteps > 250){
-      delayTime = 175;
+    else if (timeSinceLastStop > 200){
+      stepsPerNotch = 7;
     }
     else {
-      delayTime = 200;
+      stepsPerNotch = 1;
     }
+  }
+  
+  if (lastState == stateNoneTriggered && thisState != stateNoneTriggered){
+    if (thisState == stateATriggered){
+      stepsToGo += stepsPerNotch * directionFlip;
+    }
+    if (thisState == stateBTriggered){
+      stepsToGo -= stepsPerNotch * directionFlip;
+    }
+    if (stepsToGo < 0){
+      digitalWrite(directionPin, LOW);
+    }
+    else {
+      digitalWrite(directionPin, HIGH);
+    }
+    int absSteps = abs(stepsToGo);
+    delayTime = map(constrain(absSteps, stepsToGoRampStart, stepsToGoRampEnd), stepsToGoRampStart, stepsToGoRampEnd, maxStepTime, minStepTime);
+  }
+  long nowMicroSeconds = micros();
+  if (stepsToGo != 0 && nowMicroSeconds > nextStepMicrosecondsTime){
     digitalWrite(pulsePin, LOW);
-    delayMicroseconds(delayTime);
+    delayMicroseconds(100);
     digitalWrite(pulsePin, HIGH);  
-    delayMicroseconds(delayTime);
+    nextStepMicrosecondsTime = nowMicroSeconds + delayTime;
     stepsToGo += stepsToGo < 0 ? 1 : -1; 
   }
-
-
 }
